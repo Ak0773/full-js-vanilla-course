756ab95ef03d3f316659e2e36681ab73d58758a5
Ak0773 <120209368+Ak0773@users.noreply.github.com>
2025-04-08 22:20:02 +0300

    almost done Number

---
+++ 2_Numbers-and-Math/script.js
@@ -0,0 +1,56 @@
+2 + 2  // = 4
+
+10 - 3 // = 7
+
+10 * 3 // = 30
+
+10 / 2 // = 5
+
+2 + 2 + 2 // = 6
+
+2.2 + 2.2 // = 4.4
+
+10.90 * 2 + 20.95 // = 42.75
+
+10.90 * 2 + 20.95 + 4.99 // = 47.74
+
+// order of operations =  + - x /
+
+1 + 1 * 3 // = 4
+
+// operator precedence
+// first * /
+// second + -
+
+
+// if we have the same priority * /
+// calculate from left to right
+// 2 * 3 / 5
+
+// if we have the same priority + -
+// calculate from left to right
+// 2 + 3 - 5
+
+// if we use () brackets
+// calculate first the brackets
+// (1 + 1) * 3 // = 6
+
+// 20.95 + 7.99 * 2 // = 36.93
+
+// 36.93 * 0.1% // = 3.693
+
+// (20.95 + 7.99 * 2 )* 0.1% // = 3.693
+
+
+// integer = 12345
+
+// float = 1.4 2.6 3.14 4.3 5.3
+
+// binery = 001101101010010
+
+// 20.95 + 7.99 =28.9399999999998
+// (2095 + 799) / 100 = 28.94
+
+// Math.round(2.2) // = 2
+// Math.round(2.5) // = 3
+
