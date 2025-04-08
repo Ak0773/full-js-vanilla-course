e5766928f95961d60240ce943f83e4f32dd0bddc
Ak0773 <120209368+Ak0773@users.noreply.github.com>
2025-04-08 16:52:12 +0300

    learning elert and syntaxt

--- 1_JS_Basics/basics.js
+++ 1_JS_Basics/basics.js
@@ -1,6 +1,9 @@
-console.log("Hello World")
-console.log("Hello World")
-console.log("Hello World")
-console.log("Hello World")
-console.log("Hello World")
-console.log("Hello World")
+laert('hello world')
+
+alert('good job!')
+
+2 + 2  // = 4
+
+10 - 3 // = 7
+
+document.body.innerHTML = 'hello' // hello
