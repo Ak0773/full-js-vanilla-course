32f33038e55327ea9165b802794e89b82e22a083
Ak0773 <120209368+Ak0773@users.noreply.github.com>
2025-04-21 14:59:05 +0300

    Save data and generate HTML for payment summery

--- javascript-amazon-project-main/script-js/checkout.js
+++ javascript-amazon-project-main/script-js/checkout.js
@@ -1,3 +1,6 @@
 import { renderOrderSummary } from './checkout/orderSummery.js'
+import { renderOrderSummary } from './checkout/paymentSummery.js'
+
 
 renderOrderSummary()
+orderPaymrntSummary()
--- javascript-amazon-project-main/script-js/checkout/orderSummery.js
+++ javascript-amazon-project-main/script-js/checkout/orderSummery.js
@@ -1,10 +1,9 @@
 import { cart, removeFromCart, updateDeliveryOption } from '../../data/cart.js'
-import { products } from '../../data/products.js'
+import { products, getProduct } from '../../data/products.js'
 import { formatCurrency } from '../../util/money.js'
 import { hello } from 'https://unpkg.com/supersimpledev@1.0.1/hello.js'
 import dayjs from 'https://unpkg.com/supersimpledev@8.5.0/dayjs/esm/index.js'
-import { deliveryOptions } from '../../data/deliveryOptions.js'
-
+import { deliveryOptions, getDeliveryOption } from '../../data/deliveryOptions.js'
 
 hello()
 const today = dayjs()
@@ -20,24 +19,13 @@ export function renderOrderSummary() {
 	cart.forEach((cartItem) => {
 		const productId = cartItem.productId
 
-		let matchingProduct
+		const matchingProduct = getProduct(productId)
 
-		products.forEach((product) => {
-			if (productId === product.Id) {
-				matchingProduct = product
-			}
-		})
 
 		const deliveryOptionId = cartItem.deliveryOptionId
 
-		let deliveryOption
+		let deliveryOption = getDeliveryOption(deliveryOptionId)
 
-		deliveryOptions.forEach((option) => {
-			if (option.id === deliveryOptionId) {
-				deliveryOption = option
-			}
-		}
-		)
 		const today = dayjs()
 		const deliveryDate = today.add(
 			deliveryOption.deliveryDays,
