0b1d953def4c47c5e4a26e7125f808cdf42cd6fc
Ak0773 <120209368+Ak0773@users.noreply.github.com>
2025-04-21 17:51:21 +0300

    Creste automated tests for formatCurremcy

---
+++ javascript-amazon-project-main/test/moneyTest.js
@@ -0,0 +1,25 @@
+import { formatCurrency } from '../script-js/utils/money.js'
+
+console.log('converts cents into dollars')
+
+if (formatCurrency(2095) === '20.95') {
+	console.log('passed')
+} else {
+	console.log('failed')
+}
+
+console.log('works whit 0')
+
+if (formatCurrency(0) === '0.00') {
+	console.log('passed')
+} else {
+	console.log('failed')
+}
+
+console.log('round up to the nearest cent')
+
+if (formatCurrency(2000.5) === '20.01') {
+	console.log('passed')
+} else {
+	console.log('failed')
+}
